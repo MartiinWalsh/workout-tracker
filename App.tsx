7aca8de87224ec6490613f3bcf0e38b652ea250b
martinw-dev <martinwalsh.dev@gmail.com>
2023-03-18 19:12:32 +0000

    Eslint installed

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
 import React from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { HomeScreen, NewWorkoutScreen, AddWorkoutScreen } from "./Screens";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Workout Tracker" }}
+        />
+        <Stack.Screen
+          name="NewWorkoutScreen"
+          component={NewWorkoutScreen}
+          options={{ title: "New Workout" }}
+        />
+        <Stack.Screen
+          name="AddWorkoutScreen"
+          component={AddWorkoutScreen}
+          options={{ title: "Add Workout" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
